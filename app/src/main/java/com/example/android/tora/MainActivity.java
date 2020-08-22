36fd48da824f8c4c87a9d0dbd72101f0e1888523
zzy <zzy@zzydeMacBook-Air.local>
2020-08-23 06:51:39 +0800

    update on dashboard xml

--- app/src/main/java/com/example/android/tora/MainActivity.java
+++ app/src/main/java/com/example/android/tora/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.view.View;
@@ -37,9 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-        myRef.setValue("Hello, World!");
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu ) {
+        getMenuInflater().inflate( R.menu.menu, menu );
+        return true;
     }
 
     @Override
