3085a04a381841113a9284e78151dede5ec49710
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-08-22 15:25:38 -0400

    :helicopter: adding the preference work --INCOMPLETE--

--- app/src/main/java/com/example/android/tora/preferencesScreen.java
+++ app/src/main/java/com/example/android/tora/preferencesScreen.java
@@ -1,14 +1,24 @@
 package com.example.android.tora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class preferencesScreen extends AppCompatActivity {
-
+    RecyclerView dataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences_screen);
+
+        dataList = findViewById(R.id.dataList);
+
+
+
+
     }
 }
