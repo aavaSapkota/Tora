fb978e54db6f5797becd76aceb61a42d92936885
zzy <zzy@zzydeMacBook-Air.local>
2020-08-23 06:25:31 +0800

    update on dashboard xml

--- app/src/main/java/com/example/android/tora/MainActivity.java
+++ app/src/main/java/com/example/android/tora/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.android.tora;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -23,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        //create a toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.emailButton:
+                // User clicked/chose the emailButton on the top toolbar, the program will...
+                return true;
+
+            case R.id.settingsButton:
+                // User chose the "Settings" button, the program will...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
