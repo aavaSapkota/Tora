549242344632109c716493e1b301556242f3a5d3
aava <aavachhori@gmail.com>
2020-08-22 19:33:29 -0400

    next

--- app/src/main/java/com/example/android/tora/MainActivity.java
+++ app/src/main/java/com/example/android/tora/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
     @Override
     public void onStart() {
         super.onStart();
